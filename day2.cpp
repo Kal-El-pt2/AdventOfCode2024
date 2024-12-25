20fac3edf8dc6494a6bf1de20c33a88ddee011f4
Kal-El-pt2 <f20220052@hyderabad.bits-pilani.ac.in>
2024-12-25 14:47:30 +0530

    day 2 done

--- day2.cpp
+++ day2.cpp
@@ -2,32 +2,56 @@
 
 using namespace std;
 
-bool isSafe(const vector<int>& a) {
+bool isSafe(const vector<int> &a)
+{
     int n = a.size();
-    if (n < 2) return false; // Not enough levels to evaluate
+    if (n < 2)
+        return false; // Not enough levels to evaluate
     bool inc = true;
-    if(a[0] > a[1]){
+    int cnt = 1;
+    if (a[0] > a[1])
+    {
         inc = false;
     }
-    
-    for(int i=1;i<n;++i){
-        if(inc){
-            int diff = a[i] - a[i-1];
 
-            if(diff<=0 || diff >3) return false;
+    for (int i = 1; i < n; ++i)
+    {
+        if (inc)
+        {
+            int diff = a[i] - a[i - 1];
+
+            if (diff <= 0 || diff > 3)
+            {
+                if (cnt == 0)
+                    return false;
+
+                cnt--;
+            }
         }
-        else{
-            int diff = a[i-1] - a[i];
-            if(diff<=0 || diff>3) return false;
+        else
+        {
+            int diff = a[i - 1] - a[i];
+            if (diff <= 0 || diff > 3)
+            {
+                if (diff <= 0 || diff > 3)
+                {
+                    if (cnt == 0)
+                        return false;
+
+                    cnt--;
+                }
+            }
         }
     }
-    return true;   
+    return true;
 }
 
-int main() {
+int main()
+{
     string filename = "input.txt";
     ifstream inputFile(filename);
-    if (!inputFile.is_open()) {
+    if (!inputFile.is_open())
+    {
         cerr << "Error: Could not open the file " << filename << endl;
         return 1;
     }
@@ -35,16 +59,19 @@ int main() {
     int safeCount = 0;
     string line;
 
-    while (getline(inputFile, line)) {
+    while (getline(inputFile, line))
+    {
         stringstream ss(line);
         vector<int> levels;
         int level;
 
-        while (ss >> level) {
+        while (ss >> level)
+        {
             levels.push_back(level);
         }
 
-        if (isSafe(levels)) {
+        if (isSafe(levels))
+        {
             ++safeCount;
         }
     }
